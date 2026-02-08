0f8a9f2e0d0df70e898392de49ba4586a21f597c
ferpakenameyea <2834852759@qq.com>
2026-02-08 19:26:43 +0800

    Application Framework

--- Yufanbot.Client/Application.cs
+++ Yufanbot.Client/Application.cs
@@ -2,27 +2,45 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NapPlana.Core.Bot;
 using Yufanbot.Client.Config;
+using Yufanbot.Config;
 
 namespace Yufanbot.Client;
 
 public sealed class Application
 {
-    private readonly ServiceProvider _services;
+    private readonly IConfigProvider _configProvider;
     private readonly CoreConfig _coreConfig;
     private readonly NapBot _bot;
-    private readonly ILogger _logger;
+    private readonly ILogger<Application> _logger;
 
-    private Application(ServiceProvider services)
+    public Application(ServiceProvider services)
     {
-        _services = services;
-        _coreConfig = services.GetRequiredService<CoreConfig>();
-        _logger = services.GetRequiredService<LoggerFactory>().CreateLogger<Application>();
-        _bot = PlanaBotFactory.Create()
-            .SetSelfId(_coreConfig.SelfId)
-            .SetConnectionType(NapPlana.Core.Data.BotConnectionType.WebSocketClient)
-            .SetIp(_coreConfig.NapcatIP)
-            .SetPort(_coreConfig.NapcatPort)
-            .SetToken(_coreConfig.NapcatToken)
-            .Build();
+        _logger = services.GetRequiredService<ILoggerFactory>().CreateLogger<Application>();        
+        try
+        {
+            _configProvider = services.GetRequiredService<IConfigProvider>();
+            _coreConfig = _configProvider.Resolve<CoreConfig>();
+            _bot = PlanaBotFactory.Create()
+                .SetSelfId(_coreConfig.SelfId)
+                .SetConnectionType(NapPlana.Core.Data.BotConnectionType.WebSocketClient)
+                .SetIp(_coreConfig.NapcatIP)
+                .SetPort(_coreConfig.NapcatPort)
+                .SetToken(_coreConfig.NapcatToken)
+                .Build();
+        } 
+        catch (Exception e)
+        {
+            _logger.LogCritical(e, "Critical error when initializing application.");    
+            throw;
+        }
+    }
+
+    public async Task RunAsync()
+    {
+        await _bot.StartAsync();
+        while (true)
+        {
+            await Task.Delay(200);
+        }
     }
 }
--- Yufanbot.Client/Program.cs
+++ Yufanbot.Client/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Yufanbot.Client;
+using Yufanbot.Config;
+
+ServiceCollection services = new();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Debug()
+    .CreateLogger();
+
+services.AddLogging(builder =>
+{
+    builder.ClearProviders();
+    builder.AddSerilog(); 
+});
+
+services.AddSingleton<IFileReader, FileReader>();
+services.AddSingleton<IEnvironmentVariableProvider, EnvironmentVariableProvider>();
+services.AddSingleton<IConfigProvider, ConfigProvider>();
+
+var application = new Application(services.BuildServiceProvider());
+await application.RunAsync();
